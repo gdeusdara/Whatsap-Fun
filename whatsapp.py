0da3ff23884c692652476720e20dec02e7869ef6
Guilherme Banci <guibanci@gmail.com>
2020-06-07 00:31:40 -0300

    melhorando codigo

--- whatsapp.py
+++ whatsapp.py
@@ -22,7 +22,7 @@ def readContacts(fileName):
     secCol = sheet['B']
     for cell in range(len(firstCol)):
         contact_name = str(firstCol[cell].value)
-        contact_name = "\"" + contact_name + "\""
+        contact_name = "\"" + contact_name.strip() + "\""
         contact_number = str(secCol[cell].value)
         contact_number = "\"" + contact_number + "\""
 
@@ -60,47 +60,66 @@ input("Scan the QR code and then press Enter")
 # Keep a nice gap between successive messages
 # Use Keys.SHIFT + Keys.ENTER to give a new line effect in your Message
 msgToSend = [
-    [19, 33, 0,
+    [13, 16, 0,
         '''
-        Peço a sua licença, artista e produtor cultural, pois você se cadastrou na Semana Viver de Arte! Aqui é o Flavio Nardelli, fundador da Arte em Curso.
-        
-        Está no ar a *Semana Viver de Arte* e já com muito engajamento, mais de 600 comentários de artistas do país, querendo descobrir como captar muitos R$ em patrocínios para seu projeto cultural. Você que é artista/produtor(a) cultural e quer conhecer ó ÚNICO JEITO de bombar sua carreira ou projeto sem depender da sorte ou de um investidor milionário, você PRECISA assistir às aulas! Então clica nos links abaixo, e escolha a aula que você ainda não viu:
-
-        https://viverdearte.arteemcurso.com/aula-1
-
-        https://viverdearte.arteemcurso.com/aula-2
-
-        https://viverdearte.arteemcurso.com/aula-3
-
-        Caso os links nãos estejam ativos, responda essa mensagem com um “ok” para ativá-los.
-        Corre lá! Abs,\n
+     Querid@ artista e produtor(a) cultural!  ÚLTIMOS DIAS pra se inscrever no curso completo Viver de Arte! Aproveite essa oferta exclusiva: foram muitos pedidos da classe artística, então mantivemos o BÔNUS DA lista com 15 mil empresas patrocinadoras pela lei rouanet divididas por estado). https://viverdearte.arteemcurso.com/inscricoes-abertas
+     As inscrições se encerram segunda-feira, dia 08 de junho, 23:59, e não sabemos quando abriremos novamente e o preço vai subir! O curso conta com 10h gravadas, suporte diário (inclusive ajuda pra responder diligências) e lives quinzenais comigo! Um abraço. Te espero no curso Viver de Arte!\n
+     '''
+    ],
+    [12, 51, 0,
+        '''
+     Olá, artista e produtor(a) cultural!  ÚLTIMOS DIAS pra se inscrever no curso completo Viver de Arte! Aproveite essa oferta exclusiva: foram muitos pedidos da classe artística, então mantivemos o BÔNUS DA lista com 15 mil empresas patrocinadoras pela lei rouanet divididas por estado). https://viverdearte.arteemcurso.com/inscricoes-abertas
+     As inscrições serão encerradas na segunda-feira, dia 08 de junho, 23:59, e não sabemos quando abriremos novamente e o preço vai subir! O curso conta com 10h gravadas, suporte diário (inclusive ajuda pra responder diligências) e lives quinzenais comigo! Um abraço. Te espero no curso Viver de Arte!\n
         '''
     ],
     [12, 51, 0,
         '''
-        Peço a sua licença, artista e produtor cultural, pois você se cadastrou na Semana Viver de Arte! Aqui é o Flavio Nardelli, fundador da Arte em Curso.
-        Está no ar a Semana Viver de Arte e já com muito engajamento, mais de 600 comentários de artistas do país, querendo descobrir como captar muitos R$ em patrocínios para seu projeto cultural. Você que é artista/produtor(a) cultural e quer conhecer ó ÚNICO JEITO de bombar sua carreira ou projeto sem depender da sorte ou de um investidor milionário, você PRECISA assistir às aulas! Então clica nos links abaixo, e escolha a aula que você ainda não viu:
-        https://viverdearte.arteemcurso.com/aula-1
-        https://viverdearte.arteemcurso.com/aula-2
-        https://viverdearte.arteemcurso.com/aula-3
-
-        Caso os links nãos estejam ativos, responda essa mensagem com um “ok” para ativá-los
-
-        Confere lá! Abraço,\n
+     Olá novamente, querido artista e produtor(a) cultural!  ÚLTIMOS DIAS pra se inscrever no curso completo Viver de Arte! Aproveite essa oferta exclusiva: foram muitos pedidos da classe artística, então mantivemos o BÔNUS DA lista com 15 mil empresas patrocinadoras pela lei rouanet divididas por estado). https://viverdearte.arteemcurso.com/inscricoes-abertas
+     As inscrições se encerram segunda-feira, dia 08 de junho, 23:59. Nós não sabemos quando abriremos novamente, e o preço vai subir! O curso conta com *10h gravadas, suporte diário (inclusive ajuda pra responder diligências) e lives quinzenais comigo!* Um abraço. Te espero no curso Viver de Arte!\n
         '''
     ],
     [12, 51, 0,
         '''
-        Boa noite! Aqui é o Flavio Nardelli, fundador da Arte em Curso.
-        Você viu que está no ar a Semana Viver de Arte? E já com muito engajamento, mais de 600 comentários de artistas do país, querendo descobrir como captar muitos R$ em patrocínios para seu projeto cultural. Você que é artista/produtor(a) cultural e quer conhecer ó ÚNICO JEITO de bombar sua carreira ou projeto sem depender da sorte ou de um investidor milionário, você PRECISA assistir às aulas! Então clica nos links abaixo, e escolha a aula que você ainda não viu:
-        https://viverdearte.arteemcurso.com/aula-1
-        https://viverdearte.arteemcurso.com/aula-2
-        https://viverdearte.arteemcurso.com/aula-3
-
-        Responda “ok” a essa mensagem para que os links sejam ativados. Abs,\n
+     Como vai? Querid@ artista e produtor(a) cultural!  ÚLTIMOS DIAS pra se inscrever no curso completo Viver de Arte! Aproveite essa oferta exclusiva: foram muitos pedidos da classe artística, então mantivemos o BÔNUS DA lista com 15 mil empresas patrocinadoras pela lei rouanet divididas por estado). https://viverdearte.arteemcurso.com/inscricoes-abertas
+     Você pode se inscrever até segunda-feira, dia 08 de junho, 23:59. Após isso, não sabemos quando abriremos vagas novamente, e o preço vai subir! O curso conta com 10h gravadas, suporte diário (inclusive ajuda pra responder diligências) e lives quinzenais comigo! Um abraço. Te espero no curso Viver de Arte!\n
         '''
     ],
-    
+    [12, 51, 0,
+        '''
+     Alô alô, meu querid@!  ÚLTIMOS DIAS pra se inscrever no *curso completo* Viver de Arte! Aproveite essa oferta exclusiva: foram muitos pedidos da classe artística, então mantivemos o BÔNUS DA lista com 15 mil empresas patrocinadoras pela lei rouanet divididas por estado). https://viverdearte.arteemcurso.com/inscricoes-abertas
+     As inscrições se encerram segunda-feira, dia 08 de junho, 23:59, e não sabemos quando abriremos novamente e o preço vai subir! O curso vai te fornecer 10h de aulas gravadas, suporte diário (inclusive ajuda pra responder diligências) e lives quinzenais comigo! Um abraço. Estarei te esperando no curso Viver de Arte!\n
+        '''
+    ],
+    [12, 51, 0,
+        '''
+     Hoje é um excelente dia para te lembrar que estamos nos  ÚLTIMOS DIAS pra se inscrever no curso completo Viver de Arte! Aproveite essa oferta exclusiva: foram muitos pedidos da classe artística, então mantivemos o BÔNUS DA lista com 15 mil empresas patrocinadoras pela lei rouanet divididas por estado). https://viverdearte.arteemcurso.com/inscricoes-abertas
+     É possível se inscrever até segunda-feira, dia 08 de junho, 23:59. Nós não sabemos quando abriremos o curso novamente, e o preço vai subir! O curso conta com 10h gravadas, suporte diário (inclusive ajuda pra responder diligências) e lives quinzenais comigo! Um abraço. Te espero no curso Viver de Arte!\n
+        '''
+    ],
+    [12, 51, 0,
+        '''
+     Oi, novamente! Venho te lembrar que estes são os ÚLTIMOS DIAS pra se inscrever no curso completo Viver de Arte! Aproveite essa oferta exclusiva: foram muitos pedidos da classe artística, então mantivemos o BÔNUS DA lista com 15 mil empresas patrocinadoras pela lei rouanet divididas por estado). https://viverdearte.arteemcurso.com/inscricoes-abertas
+     As inscrições se encerram segunda-feira, dia 08 de junho, 23:59, e não sabemos quando abriremos novamente e o preço vai subir! Você ainda tem a chance de contar com 10h de aulas gravadas, um suporte diário (inclusive ajuda pra responder diligências) e lives quinzenais comigo! Um abraço. Te espero no curso Viver de Arte!\n
+        '''
+    ],
+    [12, 51, 0,
+        '''
+     Olá, olá, artista e produtor(a) cultural! estes são os ÚLTIMOS DIAS pra se inscrever no curso completo Viver de Arte! Aproveite essa oferta exclusiva: foram muitos pedidos da classe artística, então mantivemos o BÔNUS DA lista com 15 mil empresas patrocinadoras pela lei rouanet divididas por estado). https://viverdearte.arteemcurso.com/inscricoes-abertas
+     As inscrições estarão abertas até segunda-feira, dia 08 de junho, 23:59, e não sabemos quando abriremos novamente e o preço vai subir! O curso conta com 10h gravadas, suporte diário (inclusive ajuda pra responder diligências) e lives quinzenais comigo! Um abraço. Te espero no curso Viver de Arte!\n
+        '''
+    ],
+    [12, 51, 0,
+        '''
+     Prezado artista e produtor(a) cultural!  ÚLTIMOS DIAS pra se inscrever no curso completo Viver de Arte! Aproveite essa oferta exclusiva: foram muitos pedidos da classe artística, então mantivemos o BÔNUS DA lista com 15 mil empresas patrocinadoras pela lei rouanet divididas por estado). https://viverdearte.arteemcurso.com/inscricoes-abertas
+     Até segunda-feira, dia 08 de junho, 23:59, você ainda poderá se inscrever. Aproveite, pois nós não sabemos quando abriremos novamente e o preço vai subir! O curso conta com 10h gravadas, suporte diário (inclusive ajuda pra responder diligências) e lives quinzenais comigo! Um abraço. Te espero no curso Viver de Arte!\n
+        '''
+    ],
+    [12, 51, 0,
+        '''
+     Passei aqui para te avisar que nós estamos oficialmente nos  ÚLTIMOS DIAS pra se inscrever no curso completo Viver de Arte! Aproveite essa oferta exclusiva: foram muitos pedidos da classe artística, então mantivemos o BÔNUS DA lista com 15 mil empresas patrocinadoras pela lei rouanet divididas por estado). https://viverdearte.arteemcurso.com/inscricoes-abertas
+     As inscrições poderão ser feitas até segunda-feira, dia 08 de junho, 23:59, após isso, não sabemos quando abriremos novamente e o preço vai subir! O curso conta com *10h gravadas, suporte diário (inclusive ajuda pra responder diligências) e lives quinzenais comigo!* Um grande abraço! Te espero no curso Viver de Arte.\n
+        '''
+    ],
 ]
 
 # Count variable to identify the number of messages to be sent
@@ -108,113 +127,109 @@ count = 0
 
 sended = False
 
-while not sended:
+# Identify time
+curTime = datetime.datetime.now()
+curHour = curTime.time().hour
+curMin = curTime.time().minute
+curSec = curTime.time().second
 
-    # Identify time
-    curTime = datetime.datetime.now()
-    curHour = curTime.time().hour
-    curMin = curTime.time().minute
-    curSec = curTime.time().second
+# utility variables to tract count of success and fails
+sended = True
 
-    # if time matches then move further
-    if msgToSend[count][0]==curHour and msgToSend[count][1]==curMin and msgToSend[count][2]==curSec:
-        # utility variables to tract count of success and fails
-        sended = True
+success = 0
+sNo = 1
+failList = []
 
-        success = 0
-        sNo = 1
-        failList = []
+# Iterate over selected contacts
+for index in range(len(target_names)):
+# for target in targets:
+    print(sNo, ". Target is: " + target_names[index])
+    sNo+=1
+    try:
+        # Select the target_names[index]
+        x_arg = '//span[@title=' + target_names[index] + ' and contains(@dir, "auto")]'
+        print(x_arg)
+        # try:
+        #     wait5.until(EC.presence_of_element_located((
+        #         By.XPATH, x_arg
+        #     )))
+        # except:
+        # If contact not found, then search for it
+            # Select the Input Box
+        inp_xpath = "//div[@contenteditable='true']"
+        search_box = wait.until(EC.presence_of_element_located((
+            By.XPATH, inp_xpath)))
+        time.sleep(1)
 
-        # Iterate over selected contacts
-        for index in range(len(target_names)):
-        # for target in targets:
-            print(sNo, ". Target is: " + target_names[index])
-            sNo+=1
-            try:
-                # Select the target_names[index]
-                x_arg = '//span[@title=' + target_names[index] + ' and contains(@class, "_1wjpf")]'
-                print(x_arg)
-                # try:
-                #     wait5.until(EC.presence_of_element_located((
-                #         By.XPATH, x_arg
-                #     )))
-                # except:
-                # If contact not found, then search for it
-                    # Select the Input Box
-                inp_xpath = "//div[@contenteditable='true']"
-                search_box = wait.until(EC.presence_of_element_located((
-                    By.XPATH, inp_xpath)))
-                time.sleep(1)
+        # Send message
+        # taeget is your target_names[index] Name and msgToSend is you message
+        search_box.clear()
+        time.sleep(1)
+        search_box.send_keys(target_numbers[index].replace('"', '')) # (Uncomment it if your msg doesnt contain '\n')
+        # Link Preview Time, Reduce this time, if internet connection is Good
+        # search_box.send_keys(Keys.ENTER)
+        # searBoxPath = "//div[@data-tab='3']"
+        # wait5.until(EC.presence_of_element_located((
+        #     By.ID, "input-chatlist-search"
+        # )))
+        # inputSearchBox = driver.find_element_by_id("input-chatlist-search")
+        # time.sleep(0.5)
+        # # click the search button
+        # driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div/div[2]/div/button').click()
+        # time.sleep(1)
+        # inputSearchBox.clear()
+        # inputSearchBox.send_keys(target_names[index][1:len(target_names[index]) - 1])
+        # print('Target Searched')
+        # # Increase the time if searching a contact is taking a long time
+        # time.sleep(4)
 
-                # Send message
-                # taeget is your target_names[index] Name and msgToSend is you message
-                search_box.clear()
-                time.sleep(1)
-                search_box.send_keys(target_numbers[index].replace('"', '')) # (Uncomment it if your msg doesnt contain '\n')
-                # Link Preview Time, Reduce this time, if internet connection is Good
-                # search_box.send_keys(Keys.ENTER)
-                # searBoxPath = "//div[@data-tab='3']"
-                # wait5.until(EC.presence_of_element_located((
-                #     By.ID, "input-chatlist-search"
-                # )))
-                # inputSearchBox = driver.find_element_by_id("input-chatlist-search")
-                # time.sleep(0.5)
-                # # click the search button
-                # driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div/div[2]/div/button').click()
-                # time.sleep(1)
-                # inputSearchBox.clear()
-                # inputSearchBox.send_keys(target_names[index][1:len(target_names[index]) - 1])
-                # print('Target Searched')
-                # # Increase the time if searching a contact is taking a long time
-                # time.sleep(4)
+        time.sleep(6)
+        try:
+            driver.find_element_by_xpath(x_arg).click()
+            print("Target Successfully Selected")
+            time.sleep(2)
+        except:
+            testNumber = target_numbers[index].replace('"', '')
+            search_box.clear()
+            time.sleep(1)
+            search_box.send_keys(testNumber[3:])
+            time.sleep(6)
+            driver.find_element_by_xpath(x_arg).click()
+            print("Target Successfully Selected")
 
-                time.sleep(2)
-                try:
-                    driver.find_element_by_xpath(x_arg).click()
-                    print("Target Successfully Selected")
-                    time.sleep(2)
-                except:
-                    testNumber = target_numbers[index].replace('"', '')
-                    search_box.clear()
-                    time.sleep(1)
-                    search_box.send_keys(testNumber[3:])
-                    time.sleep(2)
-                    driver.find_element_by_xpath(x_arg).click()
-                    print("Target Successfully Selected")
+        # Select the target_names[index]
+        time.sleep(1)
+        # Select the Input Box
+        inp_xpath = '//div[@spellcheck="true"]'
+        input_box = wait.until(EC.presence_of_element_located((
+            By.XPATH, inp_xpath)))
+        time.sleep(1)
 
-                # Select the target_names[index]
-                time.sleep(1)
-                # Select the Input Box
-                inp_xpath = "//div[@spellcheck='true']"
-                input_box = wait.until(EC.presence_of_element_located((
-                    By.XPATH, inp_xpath)))
-                time.sleep(1)
+        # Send message
+        # taeget is your target_names[index] Name and msgToSend is you message
+        input_box.send_keys(msgToSend[count][3]) # + Keys.ENTER (Uncomment it if your msg doesnt contain '\n')
+        # Link Preview Time, Reduce this time, if internet connection is Good
+        time.sleep(6)
+        # input_box.send_keys(Keys.ENTER)
+        print("Successfully Send Message to : "+ target_names[index] + '\n')
+        success+=1
+        time.sleep(0.5)
 
-                # Send message
-                # taeget is your target_names[index] Name and msgToSend is you message
-                input_box.send_keys(msgToSend[count][3] + Keys.SPACE) # + Keys.ENTER (Uncomment it if your msg doesnt contain '\n')
-                # Link Preview Time, Reduce this time, if internet connection is Good
-                time.sleep(6)
-                # input_box.send_keys(Keys.ENTER)
-                print("Successfully Send Message to : "+ target_names[index] + '\n')
-                success+=1
-                time.sleep(0.5)
+        count+=1
 
-                count+=1
-
-                if count==len(msgToSend):
-                    count=0
-            except:
-                # If target_names[index] Not found Add it to the failed List
-                f = open("errorList.txt", "a")
-                f.write("%s - %s\n" % (target_names[index], target_numbers[index]))
-                f.close()
-                print("Cannot find Target: " + target_names[index] + " " + target_numbers[index])
-                failList.append(target_names[index])
+        if count==len(msgToSend):
+            count=0
+    except:
+        # If target_names[index] Not found Add it to the failed List
+        f = open("errorList.txt", "a")
+        f.write("%s - %s\n" % (target_names[index], target_numbers[index]))
+        f.close()
+        print("Cannot find Target: " + target_names[index] + " " + target_numbers[index])
+        failList.append(target_names[index])
 
 
-        print("\nSuccessfully Sent to: ", success)
-        print("Failed to Sent to: ", len(failList))
-        print(failList)
-        print('\n\n')
+print("\nSuccessfully Sent to: ", success)
+print("Failed to Sent to: ", len(failList))
+print(failList)
+print('\n\n')
 driver.quit()
